547db5ab233ee4b2091d7cde41e09d9a1c2248df
fabriziogaliano <fabrizio.galiano@hotmail.com>
2018-06-04 01:59:19 +0200

    if then try

--- src/templatePipeline.groovy
+++ src/templatePipeline.groovy
@@ -2,7 +2,9 @@
 pipeline {
     agent any
     environment {
+        DOCKER_IMAGE_BUILD_NAME = 'ciserver'
         DOCKER_REGISTRY = 'registry.zombox.it'
+        DOCKER_AWS_REGISTRY = 'registry.aws.it'
         GIT_REPOSITORY = 'https://github.com/fabriziogaliano'
         DEPLOY_SSH_TARGET = '192.168.0.109'
         DEPLOY_SSH_USER = 'root'
@@ -25,13 +27,6 @@ pipeline {
 
         stage('Docker Tag') {
             if (${GIT_REF} == "develop") {
-                environment {
-                    DOCKER_REGISTRY = 'registry.zombox.it'
-                    GIT_REPOSITORY = 'https://github.com/fabriziogaliano'
-                    DEPLOY_SSH_TARGET = '192.168.0.109'
-                    DEPLOY_SSH_USER = 'root'
-                    DEPLOY_SSH_DEFAULT_PATH = '/docker'
-                }
                 steps {
                     dockerTag()
                     echo "Tag Completed"
@@ -86,13 +81,21 @@ pipeline {
 
 def dockerBuild() {
     node {
-        sh 'docker build -t ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} .'
+        sh 'docker build -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} .'
     }
 }
 
 def dockerTag() {
     node {
-        sh 'docker tag ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
+        sh 'docker tag ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
+        sh 'docker tag ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF}'
+    }
+}
+
+def dockerAwsTag() {
+    node {
+        sh 'docker tag ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:latest'
+        sh 'docker tag ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:${GIT_REF}'
     }
 }
 
@@ -105,10 +108,22 @@ def dockerPush() {
     }
 }
 
+def dockerAwsPush() {
+    node {
+        withDockerRegistry(credentialsId: '655afa6d-5a19-4f15-97ce-29ac43336234', url: 'https://registry.aws.it') {
+        sh 'docker push ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:${GIT_REF}'
+        sh 'docker push ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:latest'
+    }
+    }
+}
+
 def cleanUp() {
     node {
         sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} '
         sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:latest '
+        sh 'docker rmi ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:${GIT_REF} '
+        sh 'docker rmi ${DOCKER_AWS_REGISTRY}/${JOB_NAME}:latest'
+        sh 'docker rmi ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REF} '
     }
 }
 
