cafe8b6ce5f7394f92181f1a7454139a6237df97
fabriziogaliano <fabrizio.galiano@hotmail.com>
2018-06-03 23:22:26 +0200

    change groovy file

--- src/templatePipeline.groovy
+++ src/templatePipeline.groovy
@@ -1,69 +1,68 @@
-def myJenkinsPipelineTemplate(Map templateParams) {
-        pipeline {
+
+pipeline {
+    agent any
+    environment {
+        DOCKER_REGISTRY = 'registry.zombox.it'
+    }
+    stages {
+        stage('Git pull') {
             agent any
-            environment {
-                DOCKER_REGISTRY = 'registry.zombox.it'
-            }
-            stages {
-                stage('Git pull') {
-                    agent any
-                    steps {
-                        checkout([$class: 'GitSCM', branches: [[name: '*/${GIT_REF}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'aad8cb5b-ddd8-47e3-a8d4-b9f128cf3fd5', url: 'https://github.com/fabriziogaliano/${JOB_NAME}.git']]])
-                        echo "Project Updated"
-                    }
-                }
-                stage('Docker Build') {
-                    steps {
-                        dockerBuild()
-                        echo "Build Completed"
-                    }
-                }
-                stage('Docker Tag') {
-                    steps {
-                        dockerTag()
-                        echo "Tag Completed"
-                    }
-                }
-                stage('Docker Push') {
-                    steps {
-                        dockerPush()
-                        echo "Push Complete"
-                    }
-                    }
-                stage('Deploy') {
-                    steps {
-                        sh 'docker-compose -f /docker/nginx_ci/docker-compose.yml up -d --force-recreate'
-                        echo "Application Deployed to ${DEPLOY_ENV}"
-                    }
-                }
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/${GIT_REF}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'aad8cb5b-ddd8-47e3-a8d4-b9f128cf3fd5', url: 'https://github.com/fabriziogaliano/${JOB_NAME}.git']]])
+                echo "Project Updated"
             }
         }
+        stage('Docker Build') {
+            steps {
+                dockerBuild()
+                echo "Build Completed"
+            }
+        }
+        stage('Docker Tag') {
+            steps {
+                dockerTag()
+                echo "Tag Completed"
+            }
+        }
+        stage('Docker Push') {
+            steps {
+                dockerPush()
+                echo "Push Complete"
+            }
+            }
+        stage('Deploy') {
+            steps {
+                sh 'docker-compose -f /docker/nginx_ci/docker-compose.yml up -d --force-recreate'
+                echo "Application Deployed to ${DEPLOY_ENV}"
+            }
+        }
+    }
+}
 
-        def dockerBuild() {
-            node {
-                sh 'docker build -t ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} .'
-            }
-        }
+def dockerBuild() {
+    node {
+        sh 'docker build -t ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} .'
+    }
+}
 
-        def dockerTag() {
-            node {
-                sh 'docker tag ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
-            }
-        }
+def dockerTag() {
+    node {
+        sh 'docker tag ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
+    }
+}
 
-        def dockerPush() {
-            node {
-                withDockerRegistry(credentialsId: '655afa6d-5a19-4f15-97ce-29ac43336234', url: 'https://registry.zombox.it') {
-                sh 'docker push ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF}'
-                sh 'docker push ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
-            }
-            }
-        }
+def dockerPush() {
+    node {
+        withDockerRegistry(credentialsId: '655afa6d-5a19-4f15-97ce-29ac43336234', url: 'https://registry.zombox.it') {
+        sh 'docker push ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF}'
+        sh 'docker push ${DOCKER_REGISTRY}/${JOB_NAME}:latest'
+    }
+    }
+}
 
-        def cleanUp() {
-            node {
-                sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} '
-                sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:latest '
-            }
-        }
+def cleanUp() {
+    node {
+        sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} '
+        sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:latest '
+    }
 }
