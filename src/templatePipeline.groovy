bb2ef3919e72cbd94d327a53264a3e7488493e4c
fabriziogaliano <fabrizio.galiano@hotmail.com>
2018-06-03 23:54:49 +0200

    change groovy file

--- src/templatePipeline.groovy
+++ src/templatePipeline.groovy
@@ -3,12 +3,16 @@ pipeline {
     agent any
     environment {
         DOCKER_REGISTRY = 'registry.zombox.it'
+        GIT_REPOSITORY = 'https://github.com/fabriziogaliano'
+        DEPLOY_SSH_TARGET = '192.168.0.109'
+        DEPLOY_SSH_USER = 'root'
+        DEPLOY_SSH_DEFAULT_PATH = '/docker'
     }
     stages {
         stage('Git pull') {
             agent any
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/${GIT_REF}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'aad8cb5b-ddd8-47e3-a8d4-b9f128cf3fd5', url: 'https://github.com/fabriziogaliano/${JOB_NAME}.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/${GIT_REF}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'aad8cb5b-ddd8-47e3-a8d4-b9f128cf3fd5', url: '${GIT_REPOSITORY}/${JOB_NAME}.git']]])
                 echo "Project Updated"
             }
         }
@@ -32,8 +36,14 @@ pipeline {
             }
         stage('Deploy') {
             steps {
-                sh 'docker-compose -f /docker/nginx_ci/docker-compose.yml up -d --force-recreate'
+                deploy()
                 echo "Application Deployed to ${DEPLOY_ENV}"
+            }
+        }
+        stage('Clean Up') {
+            stage {
+                cleanUp()
+                echo "Old images removed from CI Server"
             }
         }
     }
@@ -64,5 +74,11 @@ def cleanUp() {
     node {
         sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:${GIT_REF} '
         sh 'docker rmi ${DOCKER_REGISTRY}/${JOB_NAME}:latest '
+    }
+}
+
+def deploy() {
+    node {
+        sh 'ssh ${DEPLOY_SSH_USER}@${DEPLOY_SSH_TARGET} "docker-compose -f ${DEPLOY_SSH_DEFAULT_PATH/${JOB_NAME} up -d --force-recreate"'
     }
 }
