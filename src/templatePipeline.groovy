fcc92fc732a439cb7bd98520106122e0687b0af9
fabriziogaliano <fabrizio.galiano@hotmail.com>
2018-06-09 02:20:19 +0200

    try GIT_REV build arg docker

--- src/templatePipeline.groovy
+++ src/templatePipeline.groovy
@@ -179,18 +179,33 @@ def dockerBuild() {
     switch(env.DEPLOY_ENV) {
         case "dev":
             node {
-                sh "docker build --label GIT_REF=${GIT_REV} --build-arg buildenv=dev ${DOCKER_CUSTOM_OPT} -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} ."
+                sh "docker build 
+                --label ${env.BUILD_NUMBER} 
+                --label GIT_REF=${GIT_REV} 
+                --build-arg buildenv=dev ${DOCKER_CUSTOM_OPT} 
+                -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} 
+                ."
             }
         break
         case "demo":
             node {
-                sh "docker build --label GIT_REF=${GIT_REV} --build-arg buildenv=demo ${DOCKER_CUSTOM_OPT} -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} ."
+                sh "docker build 
+                --label ${env.BUILD_NUMBER} 
+                --label GIT_REF=${GIT_REV} 
+                --build-arg buildenv=demo ${DOCKER_CUSTOM_OPT} 
+                -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} 
+                ."
             }
         break
         // case "prod":
         default:
             node {
-                sh "docker build --label GIT_REF=${GIT_REV} --build-arg buildenv=prod ${DOCKER_CUSTOM_OPT} -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} ."
+                sh "docker build 
+                --label ${env.BUILD_NUMBER} 
+                --label GIT_REF=${GIT_REV} 
+                --build-arg buildenv=prod ${DOCKER_CUSTOM_OPT} 
+                -t ${DOCKER_IMAGE_BUILD_NAME}/${JOB_NAME}:${GIT_REV} 
+                ."
             }
     }
 }
